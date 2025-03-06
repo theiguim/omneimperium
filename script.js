373579327c6ea449a1a20f4f9b43d2ff4c99c6a6
theiguim <theiguim@gmail.com>
2025-03-05 22:14:52 -0300

    cmmt alt service

--- script.js
+++ script.js
@@ -188,45 +188,6 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 
 
-    const serviceLinks = document.querySelectorAll(".service-link");
-    const serviceElements = document.querySelectorAll(".services-element");
-
-    let lastIndex = 0;
-
-    serviceLinks.forEach((link, index) => {
-        link.addEventListener("click", () => {
-            if (index === lastIndex) return;
-
-            // Remove a classe 'active' dos elementos anteriores
-            serviceElements.forEach((element) => element.classList.remove("active"));
-            serviceLinks.forEach((el) => el.classList.remove("active")); // Removendo o destaque do link anterior
-
-            const currentService = serviceElements[index];
-
-            // Alterar a posição do background dinamicamente para criar o efeito de deslocamento
-            if (index > lastIndex) {
-                currentService.style.backgroundPosition = "center right";
-            } else {
-                currentService.style.backgroundPosition = "center left";
-            }
-
-            setTimeout(() => {
-                currentService.style.backgroundPosition = "center center";
-                currentService.classList.add("active");
-                serviceLinks[index].classList.add("active"); // Destacar o link selecionado
-            }, 50);
-
-            lastIndex = index;
-        });
-    });
-
-    // Exibir a primeira div por padrão e destacar o primeiro link
-    serviceElements[0].classList.add("active");
-    serviceLinks[0].classList.add("active");
-
-
-
-
     const questions = document.querySelectorAll(".faq-question");
 
     questions.forEach(question => {
